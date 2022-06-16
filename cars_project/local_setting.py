449ed07d1d00c2a9b5dbacc61749813db5cccd71
Sydney McGee <mcgee.sydney@gmail.com>
2022-06-16 09:20:48 -0500

    Created Cars app

---
+++ cars_project/local_setting.py
@@ -0,0 +1,13 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-aedi5btc!_55j^5^f9*4cxx+#p1xyw0)#rg+j%5x3uhxbn=h6v'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Skadbal1800',
+    }
+}
--- cars_project/settings.py
+++ cars_project/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aedi5btc!_55j^5^f9*4cxx+#p1xyw0)#rg+j%5x3uhxbn=h6v'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +73,8 @@ WSGI_APPLICATION = 'cars_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+
 
 
 # Password validation
@@ -121,3 +117,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from cars_project.local_setting import *
+except ImportError:
+    pass
